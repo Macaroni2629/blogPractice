b0f88f1f50f506881960ea9845b7844c6888d26a
Rona Hsu <ronahsu@gmail.com>
2024-02-25 15:21:25 -0600

    blog progress

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Get express, an installed package from node_modules.
 const express = require("express");
 
-// Create an express application that'll
-// handle http requests and http responses.
+// Create an express application.
 const app = express();
 
-// If this file is hosted on a remote server (e.g. Heroku),
-// use process.env.PORT, which should be 80, the port used
-// for HTTP traffic. If no such port is defined, it means
-// we're running on our local machine. In that case, use
-// port 8000, which is commonly used for local development.
+// NEW: Get our database engine, sqlite3, and load (or create) the database file at `blog.db`
+const sqlite3 = require('sqlite3');
+const database = new sqlite3.Database('./blog.db');
+
+// Tell the application which port to use.
 const port = process.env.PORT || 8000;
 
-// Tell the app to listen for incoming requests on
-// the desired port. After we know the app is listening,
-// log a message so that we know it's working.
+// Tell the app to listen for incoming requests on the desired port.
 app.listen(port, function () {
   console.log("App listening on port: " + port);
 });
 
-// Associate the url path "/" with a given function.
-// Each time "/" is requested, the associated
-// function will run. In this case, the function
-// responds with our HTML string.
-app.get("/", function (request, response) {
-  response.send(HTML);
-});
+// NEW: Run SQL Query to create Posts table (if it doesn't already exist)
+// This will specify each field and what type of data it should hold
+database.run(`CREATE TABLE IF NOT EXISTS Posts( 
+  id INTEGER PRIMARY KEY, 
+  title TEXT, 
+  subtitle TEXT, 
+  body TEXT
+)`);
 
-// HTML for the only webpage in our application.
-const HTML = `<h1>Welcome to NAME's site!</h1>`
+// Associate the url path "/" with a given function.
+app.get("/", function (request, response) {
+  response.send("<h1>Welcome to Simple Blog!</h1>");
+});
